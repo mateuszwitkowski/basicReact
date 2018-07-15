be3c104dd79c7e102f2e7f165ab7bd75d3397dce
Mateusz Witkowski <mtkowski@gmail.com>
2018-07-15 16:33:32 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import PlayersList from './components/PlayersList';
+
+class App extends Component {
+  render() {
+    return (
+      <PlayersList/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PlayersList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlayersList extends React.Component{
+
+  render() {
+      return (
+          <div>
+              There's the list!
+          </div>
+      )
+  }
+
+}
+
+export default PlayersList;
